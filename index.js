2adfbc656e1ca7c476e5e0920dda4b953f871a1f
Alberto Miranda <codealchemist@gmail.com>
2016-09-08 01:10:53 -0300

    added heroku runtime

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var http = require('http');
+var gzippo = require('gzippo');
+
+var app = express();
+app.use(gzippo.staticGzip('' + __dirname + '/dist/'));
+app.use('/*', function(req, res){
+  res.sendfile(__dirname + '/src/build/index.html');
+});
+
+var server = http.createServer(app);
+server.listen(process.env.PORT || 5000);
