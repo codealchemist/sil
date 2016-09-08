2b21efebd96989f97fdcd2980238584a826111ed
Alberto Miranda <codealchemist@gmail.com>
2016-09-08 01:27:08 -0300

    fixed heroku run path

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var gzippo = require('gzippo');
 var app = express();
 app.use(gzippo.staticGzip('' + __dirname + '/dist/'));
 app.use('/*', function(req, res){
-  res.sendfile(__dirname + '/src/build/index.html');
+  res.sendfile(__dirname + '/build/index.html');
 });
 
 var server = http.createServer(app);
